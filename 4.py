5194c2c33138a6985367355e3873ddafd7da1bb7
Sobur khan <soburkhan7@gmail.com>
2025-10-12 17:00:03 +0300

    print name

--- 4.py
+++ 4.py
@@ -1,3 +1,7 @@
-#Print your name
-name = 'Sobur' 
-print('name')
+#Problem statement:
+'''print your name'''
+#solution
+a = input('give your first name : ')
+b=  input('give your last name : ')
+c = (a+' '+b)
+print(f'this is my full name:{c}')
