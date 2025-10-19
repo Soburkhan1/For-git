d287ac810e2e61c478b6d5aacefa650ebf23a1f0
Sobur khan <soburkhan7@gmail.com>
2025-10-19 17:29:00 +0300

    round decimal

---
+++ 6.py
@@ -0,0 +1,5 @@
+#print last character of a string name Finland
+word = 'Finland'
+last_char = word[-1]
+
+print('The last character is:', last_char)
---
+++ 7.py
@@ -0,0 +1,4 @@
+#Round 3.145622 to 2 decimal
+a = 3.145622
+x = round(a,2)
+print(x)
